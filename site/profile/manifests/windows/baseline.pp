e9b47c1c9d83b7a38cbfa83342c0aed8b17f63d3
SeanTecha <sean.t@puppet.com>
2017-01-18 14:30:01 -0800

    installing dotnet

--- site/profile/manifests/windows/baseline.pp
+++ site/profile/manifests/windows/baseline.pp
@@ -1,12 +1,10 @@
 class profile::windows::baseline {
 
-  dotnet { 'dotnet35': version => '3.5' }
-  include chocolatey
-
   Package {
     ensure   => installed,
-    provider => chocolatey,
   }
+  
+  include chocolate,
 
   package { 'unzip': }
   package { 'git': }
