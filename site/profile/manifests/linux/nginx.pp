4ecbce8b6321c420c719bbae552b404954ca9f94
SeanTecha <sean.t@puppet.com>
2017-01-18 16:31:17 -0800

    facter syntax?

--- site/profile/manifests/linux/nginx.pp
+++ site/profile/manifests/linux/nginx.pp
@@ -4,7 +4,7 @@ class profile::linux::nginx {
     package_source => 'nginx-stable',
   }
 
-  nginx::resource::server{ '$::ipaddress':
+  nginx::resource::server{ $::ipaddress:
     www_root => '/opt/html/',
   }
 }
