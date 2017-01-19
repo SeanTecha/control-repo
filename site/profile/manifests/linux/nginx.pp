d742d5d66b352a6732d839633df9366134da05be
SeanTecha <sean.t@puppet.com>
2017-01-19 10:50:02 -0800

    changing www_root

--- site/profile/manifests/linux/nginx.pp
+++ site/profile/manifests/linux/nginx.pp
@@ -2,9 +2,6 @@ class profile::linux::nginx {
   class{ 'nginx':
     manage_repo    => true,
     package_source => 'nginx-stable',
-  }
-
-  nginx::resource::server{ 'localhost':
-    www_root => '/var/www/',
+    www_root => 'var/www',
   }
 }
