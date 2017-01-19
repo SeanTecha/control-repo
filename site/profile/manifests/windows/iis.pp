2f5d1f5abd3fe74db1ef9c8dd27fd231f0e2d79c
SeanTecha <sean.t@puppet.com>
2017-01-19 13:56:47 -0800

    testing iis

--- site/profile/manifests/windows/iis.pp
+++ site/profile/manifests/windows/iis.pp
@@ -1,4 +1,14 @@
 class profile::windows::iis {
+
+  dsc_windowsfeature{'iis':
+    dsc_ensure => 'Present',
+    dsc_name   => 'Web-Server',
+  }
+  dsc_windowsfeature{'aspnet45':
+    dsc_ensure => 'Present',
+    dsc_name   => 'Web-Asp-Net45',
+  }
+
   iis::manage_app_pool { 'my_application_pool':
     enable_32_bit           => true,
     managed_runtime_version => 'v4.0',
